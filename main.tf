04433af97953dec8d67947c3f877bddaa73086e0
19Obed <106348524+19Obed@users.noreply.github.com>
2022-05-26 21:49:35 +0100

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,142 @@
+#creating aws network for project 
+
+resource "aws_vpc" "Cloud_engine" {
+  cidr_block           = var.vpc-cidr
+  instance_tenancy     = "default"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+
+  tags = {
+    Name = "cloud_engine"
+  }
+}
+
+#public subnet 
+resource "aws_subnet" "cloud_subnet" {
+  vpc_id            = aws_vpc.Cloud_engine.id
+  cidr_block        = var.public-cidr
+  availability_zone = var.az1
+
+  tags = {
+    Name = "cloud_engine"
+  }
+}
+
+resource "aws_subnet" "cloud_subnet_2" {
+  vpc_id            = aws_vpc.Cloud_engine.id
+  cidr_block        = var.public-cidr_2
+  availability_zone = var.az2
+
+  tags = {
+    Name = "cloud_engine_2"
+  }
+}
+
+resource "aws_subnet" "cloud_subnet_3" {
+  vpc_id            = aws_vpc.Cloud_engine.id
+  cidr_block        = var.public-cidr_3
+  availability_zone = var.az3
+
+  tags = {
+    Name = "cloud_engine_3"
+  }
+}
+
+#private subnet 
+resource "aws_subnet" "cloud_private_subnet" {
+  vpc_id            = aws_vpc.Cloud_engine.id
+  cidr_block        = var.private-cidr
+  availability_zone = var.az4
+
+  tags = {
+    Name = "cloud_private_subnet"
+  }
+}
+
+resource "aws_subnet" "cloud_private_subnet_2" {
+  vpc_id            = aws_vpc.Cloud_engine.id
+  cidr_block        = var.private-cidr_2
+  availability_zone = var.az5
+
+  tags = {
+    Name = "cloud_private_subnet_2"
+  }
+}
+
+#public route table
+resource "aws_route_table" "cloud_route_table" {
+  vpc_id = aws_vpc.Cloud_engine.id
+
+
+  tags = {
+    Name = "cloud_route_table"
+  }
+}
+
+#private route table
+resource "aws_route_table" "cloud_private_route_table" {
+  vpc_id = aws_vpc.Cloud_engine.id
+
+
+  tags = {
+    Name = "cloud_private_route_table"
+  }
+}
+
+#associate public subnet with the public route table
+resource "aws_route_table_association" "cloud_route_table_association" {
+  subnet_id      = aws_subnet.cloud_subnet.id
+  route_table_id = aws_route_table.cloud_route_table.id
+}
+
+#associate private sunet with private route table
+resource "aws_route_table_association" "cloud_private_route_table_association" {
+  subnet_id      = aws_subnet.cloud_private_subnet.id
+  route_table_id = aws_route_table.cloud_private_route_table.id
+}
+
+#internet gateway
+resource "aws_internet_gateway" "cloud_IGW" {
+  vpc_id = aws_vpc.Cloud_engine.id
+
+  tags = {
+    Name = "cloud_IGW"
+  }
+}
+
+#associate internet gateway to the public route table 
+resource "aws_route" "cloud_IGW_association" {
+  route_table_id         = aws_route_table.cloud_route_table.id
+  gateway_id             = aws_internet_gateway.cloud_IGW.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+#elastic eip addresss
+resource "aws_eip" "cloud_engine_EIP" {
+  vpc = true
+
+  tags = {
+    "Name" = "cloud_engine_eip"
+  }
+}
+
+#Nat gateway associated with public subnet
+resource "aws_nat_gateway" "cloud_NGW_pubsub" {
+  allocation_id = aws_eip.cloud_engine_EIP.id
+  subnet_id     = aws_subnet.cloud_subnet.id
+
+  tags = {
+    Name = "cloud_NGW_pubsub"
+  }
+}
+
+#NAT gateway associated with private subnet
+resource "aws_nat_gateway" "cloud_NGW_pubsub_private" {
+  connectivity_type = "private"
+  subnet_id         = aws_subnet.cloud_private_subnet.id
+
+  tags = {
+    Name = "cloud_NGW_pubsub_private"
+  }
+}
---
+++ variable.tf
@@ -0,0 +1,83 @@
+variable "vpc-cidr" {
+  type        = string
+  description = "Name of vpc cdr"
+  default     = "10.0.0.0/16"
+
+}
+
+variable "public-cidr" {
+  type        = string
+  description = "Name of public subnet cidr"
+  default     = "10.0.1.0/24"
+
+}
+
+variable "public-cidr_2" {
+  type        = string
+  description = "Name of public subnet 2 cidr"
+  default     = "10.0.2.0/24"
+
+}
+
+variable "public-cidr_3" {
+  type        = string
+  description = "Name of public subnet 3 cidr"
+  default     = "10.0.3.0/24"
+
+}
+
+variable "private-cidr" {
+  type        = string
+  description = "Name of private subnet cidr"
+  default     = "10.0.4.0/24"
+
+}
+
+variable "private-cidr_2" {
+  type        = string
+  description = "Name of private subnet 2 cidr"
+  default     = "10.0.5.0/24"
+
+}
+
+variable "az1" {
+  type        = string
+  description = "Name of availabilty zone cidr"
+  default     = "eu-west-1a"
+
+}
+
+variable "az2" {
+  type        = string
+  description = "Name of availabilty zone cidr"
+  default     = "eu-west-1b"
+
+}
+
+variable "az3" {
+  type        = string
+  description = "Name of availabilty zone cidr"
+  default     = "eu-west-1c"
+
+}
+
+variable "az4" {
+  type        = string
+  description = "Name of availabilty zone cidr"
+  default     = "eu-west-1b"
+
+}
+
+variable "az5" {
+  type        = string
+  description = "Name of availabilty zone cidr"
+  default     = "eu-west-1c"
+
+}
+
+variable "az-provider" {
+  type        = string
+  description = "Name of availabilty zone cidr"
+  default     = "eu-west-1"
+
+}
